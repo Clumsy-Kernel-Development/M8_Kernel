90765e1679bea0f4317d9559183c2c1e79c5ef2c
Thomas Roberts <tom.p.roberts@gmail.com>
2015-05-16 19:59:28 +0100

    first commit - m8ul-3.4.0 with kcal

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Fri May 15 23:14:40 BST 2015"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "LeeDrOiD"
+#define LINUX_COMPILER "gcc version 4.6 20120106 (prerelease) (GCC) "
