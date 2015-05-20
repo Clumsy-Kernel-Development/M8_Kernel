d87eef0cca5e348624529863ef93830790156b76
Thomas Roberts <tom.p.roberts@gmail.com>
2015-05-20 19:15:41 +0100

    Fixed Compile erros for Linaro Toolchain

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 6 */
+/* This file is auto generated, version 7 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#6 SMP PREEMPT Sun May 17 19:14:40 BST 2015"
+#define UTS_VERSION "#7 SMP PREEMPT Wed May 20 19:09:59 BST 2015"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "LeeDrOiD"
-#define LINUX_COMPILER "gcc version 4.6 20120106 (prerelease) (GCC) "
+#define LINUX_COMPILER "gcc version 4.9.3 20150303 (release) [ARM/embedded-4_9-branch revision 221220] (GNU Tools for ARM Embedded Processors) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.0-LeeDrOiD-M8-1.00-g5e4b8a2-dirty"
+#define UTS_RELEASE "3.4.0-LeeDrOiD-M8-1.00-gc70efda-dirty"
