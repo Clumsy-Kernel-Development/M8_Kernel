6c98c716adc038e7f19185f0615c440b0dc8e56a
Thomas Roberts <tom.p.roberts@gmail.com>
2015-06-26 14:17:37 +0100

    Clean Up

--- .missing-syscalls.d
+++
@@ -1,7 +0,0 @@
--:  /home/tom/xda/kernel/M8_Kernel/include/linux/kconfig.h \
- include/generated/autoconf.h \
- /home/tom/xda/kernel/M8_Kernel/arch/arm/include/asm/types.h \
- include/asm-generic/int-ll64.h \
- arch/arm/include/generated/asm/bitsperlong.h \
- include/asm-generic/bitsperlong.h \
- /home/tom/xda/kernel/M8_Kernel/arch/arm/include/asm/unistd.h
--- scripts/mod/.empty.o.cmd
+++
@@ -1,9 +0,0 @@
-cmd_scripts/mod/empty.o := /home/tom/xda/kernel/M8_Kernel/scripts/gcc-wrapper.py arm-none-eabi-gcc -Wp,-MD,scripts/mod/.empty.o.d  -nostdinc -isystem /usr/bin/../lib/gcc/arm-none-eabi/4.9.3/include -I/home/tom/xda/kernel/M8_Kernel/arch/arm/include -Iarch/arm/include/generated -Iinclude  -include /home/tom/xda/kernel/M8_Kernel/include/linux/kconfig.h -D__KERNEL__ -mlittle-endian -Iarch/arm/mach-msm/include -Wall -Wundef -Wstrict-prototypes -Wno-trigraphs -fno-strict-aliasing -fno-common -Werror-implicit-function-declaration -Wno-format-security -fno-delete-null-pointer-checks -O2 -marm -fno-dwarf2-cfi-asm -fstack-protector -mabi=aapcs-linux -mno-thumb-interwork -funwind-tables -D__LINUX_ARM_ARCH__=7 -mcpu=cortex-a15 -msoft-float -Uarm -Wframe-larger-than=1024 -Wno-unused-but-set-variable -fomit-frame-pointer -g -Wdeclaration-after-statement -Wno-pointer-sign -fno-strict-overflow -fconserve-stack -DCC_HAVE_ASM_GOTO    -D"KBUILD_STR(s)=\#s" -D"KBUILD_BASENAME=KBUILD_STR(empty)"  -D"KBUILD_MODNAME=KBUILD_STR(empty)" -c -o scripts/mod/.tmp_empty.o scripts/mod/empty.c
-
-source_scripts/mod/empty.o := scripts/mod/empty.c
-
-deps_scripts/mod/empty.o := \
-
-scripts/mod/empty.o: $(deps_scripts/mod/empty.o)
-
-$(deps_scripts/mod/empty.o):
