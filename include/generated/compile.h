e2095529af3f152c1d6f580378c39fb9a8ef02ce
Thomas Roberts <tom.p.roberts@gmail.com>
2015-06-27 13:54:13 +0100

    Add dts files for all-variant support

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Fri Jun 26 14:25:35 BST 2015"
+#define UTS_VERSION "#1 SMP PREEMPT Sat Jun 27 13:42:20 BST 2015"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "clumsy"
 #define LINUX_COMPILER "gcc version 4.9.3 20150303 (release) [ARM/embedded-4_9-branch revision 221220] (GNU Tools for ARM Embedded Processors) "
